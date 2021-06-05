71f533e85c1d15fce9a7ac029b6430baab884e47
naman.sharma <naman.sharma1@xebia.com>
2021-06-05 13:38:32 +0530

    New Commit

--- src/main/java/com/cowin/vaccination/service/impl/AvailabilityServiceImpl.java
+++ src/main/java/com/cowin/vaccination/service/impl/AvailabilityServiceImpl.java
@@ -1,14 +1,12 @@
 package com.cowin.vaccination.service.impl;
 
 import com.cowin.vaccination.service.AvailabilityService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Slf4j
 @Service
 public class AvailabilityServiceImpl implements AvailabilityService {
 
